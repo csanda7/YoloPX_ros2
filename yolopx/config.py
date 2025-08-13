32708eaf6dd099042703c970160c39c685a8d43a
Norbert Csanda <csandanorbert@gmail.com>
2025-08-13 10:10:08 +0200

    Code cleaned and structure change

---
+++ yolopx/config.py
@@ -0,0 +1,19 @@
+# ------------------------------ Model & IO ---------------------------------
+ENGINE: str = "/home/csanda/Work/JKK/ros2/yolopx/models/yolopx_2.engine"
+TOPIC: str = "/zed2i/zed_node/left/image_rect_color"
+RELIABLE: bool = False  
+SHOW: bool = True       #opencv ablak
+
+# ------------------------------ Thresholds ---------------------------------
+LANE_THRESH: float = 0.70
+DRIVE_THRESH: float = 0.80
+
+# ------------------------------ Input size ---------------------------------
+INPUT_W: int = 640
+INPUT_H: int = 640
+DEFAULT_INPUT_SHAPE = (1, 3, INPUT_H, INPUT_W)
+
+# ------------------------------ UI / timing --------------------------------
+WINDOW_TITLE: str = "YOLOPX Inference"
+WATCHDOG_PERIOD_SEC: float = 1.0
+NO_FRAME_TIMEOUT_SEC: float = 2.5
